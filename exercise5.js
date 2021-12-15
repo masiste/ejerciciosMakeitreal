d3a34a533c276011e4af1256c033a522887e972b
masiste <acevedojh@gmail.com>
2021-12-15 06:52:42 -0500

    detalles adicionales comentarios exercise5

--- exercise5.js
+++ exercise5.js
@@ -1,5 +1,18 @@
-const peso = parseInt(prompt("Ingrese su peso: "))
+/*
+El índice de masa corporal (IMC), o BMI por sus siglas en inglés, 
+es un valor que determina la cantidad de grasa de una persona.
+
+El BMI se calcula con la siguiente formula: peso / altura^2
+
+Escribe un programa que le pida al usuario su peso y su altura 
+para calcular su BMI e imprima la frase "Tu BMI es X".
+
+Por ejemplo, si el usuario ingresa 65 de peso y 1.8 de altura el
+programa debe imprimir en la consola la frase "Tu BMI es 20.061728395061728"
+*/
+const peso = parseFloat(prompt("Ingrese su peso: "))
 const altura = parseFloat(prompt("Ingrese su altura"))
 let imc = parseFloat(peso / Math.pow(altura,2))
+
 
 console.log("Su indice de masa corporal es: " +imc)
