c11a388b0d051161e32e18d277bc6edae8621d1e
masiste <acevedojh@gmail.com>
2021-12-17 12:15:13 -0500

    ed

--- exercise7.js
+++ exercise7.js
@@ -2,3 +2,16 @@
 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola 
 "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!" */
 
+
+function randomGenerate(min, max){
+    return Math.floor(Math.random()*(max-min))+min;}
+
+const numberDicern = randomGenerate(1,10)
+console.log("el numero generado es " +numberDicern)
+const numberEnter = parseInt(prompt("What is the number ? "))
+
+while (numberEnter != numberDicern) {
+    numberEnter = parseInt(prompt("Intentalo de nuevo "))
+}
+console.log("Felicitaciones ese era")
+
